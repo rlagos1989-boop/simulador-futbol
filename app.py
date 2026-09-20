bbb24fe53a57d9e88d7d0f02bd659ca49ef6186c
rlagos1989-boop <rlagos1989@gmail.com>
2026-09-20 16:55:40 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(
-    title="Simulador Monte Carlo Predictivo Ultra Pro 2026/2027", layout="wide"
+    page_title="Simulador Monte Carlo Predictivo Ultra Pro 2026/2027", layout="wide"
 )
 
 ARCH_HISTORIAL = "registro_pronosticos.csv"
